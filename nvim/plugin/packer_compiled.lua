ce905deecc7569f9deb2bab1e02654b29ce69066
Caleb Ekstrand <ekshore@GLaDOS.local>
2023-06-18 00:36:23 -0500

    Added git ignore

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -89,6 +89,12 @@ _G.packer_plugins = {
     path = "/Users/ekshore/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
+  ["highlight-whitespace"] = {
+    config = { "\27LJ\2\nè\2\0\0\5\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0035\4\a\0=\4\b\3=\3\t\2B\0\2\1K\0\1\0\17user_palette\nother\1\0\4\t\\t\\+\nplum4\btws\18PaleVioletRed\25\\(\\S\\)\\@<=\\s\\(,\\)\\@=\vcoral1\31\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@=\20LightGoldenrod3\rmarkdown\1\0\0\1\0\4\29\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@=\15CadetBlue3\btws\14RosyBrown\31\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@=\rSkyBlue1\t\\t\\+\nplum4\1\0\2\btws\n\\s\\+$\22clear_on_winleave\1\nsetup\25highlight-whitespace\frequire\0" },
+    loaded = true,
+    path = "/Users/ekshore/.local/share/nvim/site/pack/packer/start/highlight-whitespace",
+    url = "https://github.com/lukoshkin/highlight-whitespace"
+  },
   ["lsp-zero.nvim"] = {
     loaded = true,
     path = "/Users/ekshore/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
@@ -154,12 +160,6 @@ _G.packer_plugins = {
     path = "/Users/ekshore/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
-  ["trailing-whitespace"] = {
-    config = { "\27LJ\2\n¥\1\0\0\4\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\2B\0\2\1K\0\1\0\fpalette\1\0\1\rmarkdown\14RosyBrown\rpatterns\1\0\1\18default_color\18PaleVioletRed\1\2\0\0\n\\s\\+$\nsetup\24trailing-whitespace\frequire\0" },
-    loaded = true,
-    path = "/Users/ekshore/.local/share/nvim/site/pack/packer/start/trailing-whitespace",
-    url = "https://github.com/lukoshkin/trailing-whitespace"
-  },
   undotree = {
     loaded = true,
     path = "/Users/ekshore/.local/share/nvim/site/pack/packer/start/undotree",
@@ -173,10 +173,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: trailing-whitespace
-time([[Config for trailing-whitespace]], true)
-try_loadstring("\27LJ\2\n¥\1\0\0\4\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\2B\0\2\1K\0\1\0\fpalette\1\0\1\rmarkdown\14RosyBrown\rpatterns\1\0\1\18default_color\18PaleVioletRed\1\2\0\0\n\\s\\+$\nsetup\24trailing-whitespace\frequire\0", "config", "trailing-whitespace")
-time([[Config for trailing-whitespace]], false)
+-- Config for: highlight-whitespace
+time([[Config for highlight-whitespace]], true)
+try_loadstring("\27LJ\2\nè\2\0\0\5\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0035\4\a\0=\4\b\3=\3\t\2B\0\2\1K\0\1\0\17user_palette\nother\1\0\4\t\\t\\+\nplum4\btws\18PaleVioletRed\25\\(\\S\\)\\@<=\\s\\(,\\)\\@=\vcoral1\31\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@=\20LightGoldenrod3\rmarkdown\1\0\0\1\0\4\29\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@=\15CadetBlue3\btws\14RosyBrown\31\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@=\rSkyBlue1\t\\t\\+\nplum4\1\0\2\btws\n\\s\\+$\22clear_on_winleave\1\nsetup\25highlight-whitespace\frequire\0", "config", "highlight-whitespace")
+time([[Config for highlight-whitespace]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
