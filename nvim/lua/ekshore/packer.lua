5117dd30e08b65fad42e92b3673e2044969e2fc1
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2023-06-20 16:20:50 -0500

    Added Minimap support

--- nvim/lua/ekshore/packer.lua
+++ nvim/lua/ekshore/packer.lua
@@ -12,7 +12,10 @@ return require('packer').startup(function(use)
     -- Gruv Box theme for NeoVim
     use { "ellisonleao/gruvbox.nvim" }
     -- Code Minimap
-    use('wfxr/minimap.vim')
+    use {
+        'wfxr/minimap.vim',
+        run = ':!cargo install --locked code-minimap'
+    }
     -- Treesitter for AST and code coloring
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
@@ -46,34 +49,34 @@ return require('packer').startup(function(use)
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/nvim-cmp' },     -- Required
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
         }
     }
 
     -- Highlight Trailing whitespace
---    use {
---        'lukoshkin/highlight-whitespace',
---        config = function()
---            require 'highlight-whitespace'.setup {
---                tws = '\\s\\+$',
---                clear_on_winleave = false,
---                user_palette = {
---                    markdown = {
---                        tws = 'RosyBrown',
---                        ['\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@='] = 'CadetBlue3',
---                        ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'SkyBlue1',
---                        ['\\t\\+'] = 'plum4',
---                    },
---                    other = {
---                        tws = 'PaleVioletRed',
---                        ['\\(\\S\\)\\@<=\\s\\(,\\)\\@='] = 'coral1',
---                        ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'LightGoldenrod3',
---                        ['\\t\\+'] = 'plum4',
---                    }
---                }
---            }
---        end
---    }
+    --    use {
+    --        'lukoshkin/highlight-whitespace',
+    --        config = function()
+    --            require 'highlight-whitespace'.setup {
+    --                tws = '\\s\\+$',
+    --                clear_on_winleave = false,
+    --                user_palette = {
+    --                    markdown = {
+    --                        tws = 'RosyBrown',
+    --                        ['\\(\\S\\)\\@<=\\s\\(\\.\\|,\\)\\@='] = 'CadetBlue3',
+    --                        ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'SkyBlue1',
+    --                        ['\\t\\+'] = 'plum4',
+    --                    },
+    --                    other = {
+    --                        tws = 'PaleVioletRed',
+    --                        ['\\(\\S\\)\\@<=\\s\\(,\\)\\@='] = 'coral1',
+    --                        ['\\(\\S\\)\\@<= \\{2,\\}\\(\\S\\)\\@='] = 'LightGoldenrod3',
+    --                        ['\\t\\+'] = 'plum4',
+    --                    }
+    --                }
+    --            }
+    --        end
+    --    }
 end)
